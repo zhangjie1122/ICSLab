9c458a496bbe2a5d005758a596367e7f5c142ee1
Jie Zhang <1003440965@qq.com>
2015-11-10 19:12:13 +0800

    ics lab1 source code

---
+++ datalab-handout/bits.h
@@ -0,0 +1,17 @@
+
+int bitOr(int, int);
+int test_bitOr(int, int);
+int getByte(int, int);
+int test_getByte(int, int);
+int logicalShift(int, int);
+int test_logicalShift(int, int);
+int bitParity(int);
+int test_bitParity(int);
+int fitsBits(int, int);
+int test_fitsBits(int, int);
+int negate(int);
+int test_negate(int);
+int isPositive(int);
+int test_isPositive(int);
+int addOK(int, int);
+int test_addOK(int, int);
---
+++ datalab-handout/decl.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define TMin INT_MIN
+#define TMax INT_MAX
+
+#include "btest.h"
+#include "bits.h"
+
+test_rec test_set[] = {
+
+
+
+
+
+ {"bitOr", (funct_t) bitOr, (funct_t) test_bitOr, 2, "& ~", 8, 1,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"getByte", (funct_t) getByte, (funct_t) test_getByte, 2,
+    "! ~ & ^ | + << >>", 6, 2,
+  {{TMin, TMax},{0,3},{TMin,TMax}}},
+ {"logicalShift", (funct_t) logicalShift, (funct_t) test_logicalShift,
+   2, "! ~ & ^ | + << >>", 20, 3,
+  {{TMin, TMax},{0,31},{TMin,TMax}}},
+ {"bitParity", (funct_t) bitParity, (funct_t) test_bitParity, 1, "! ~ & ^ | + << >>", 20, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"fitsBits", (funct_t) fitsBits, (funct_t) test_fitsBits, 2,
+    "! ~ & ^ | + << >>", 15, 2,
+  {{TMin, TMax},{1,32},{TMin,TMax}}},
+ {"negate", (funct_t) negate, (funct_t) test_negate, 1,
+    "! ~ & ^ | + << >>", 5, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isPositive", (funct_t) isPositive, (funct_t) test_isPositive, 1,
+    "! ~ & ^ | + << >>", 8, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"addOK", (funct_t) addOK, (funct_t) test_addOK, 2,
+    "! ~ & ^ | + << >>", 20, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+  {"", NULL, NULL, 0, "", 0, 0,
+   {{0, 0},{0,0},{0,0}}}
+};
